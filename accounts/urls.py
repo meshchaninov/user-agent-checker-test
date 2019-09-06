2d2029530c48a129ace63031d649177ec9b0eb8f
nmm33342 <meshchaninov.n@gmail.com>
2019-09-06 13:17:39 +0700

    Доавил logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,6 @@ from django.contrib.auth import views
 
 
 urlpatterns = [
-    path('login/', views.LoginView.as_view(), name='login')
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout')
 ]
